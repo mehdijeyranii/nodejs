7d6d0021bdb1302644115d0a3c800dba531e7e7f
Mehdi Jeyrani <mehdijeyrani@gmail.com>
2025-03-27 21:15:40 +0330

    Import specific function from calculator module

--- app.js
+++ app.js
@@ -9,3 +9,9 @@ const math = require("./math");
 
 console.log(math.add(15, 5)); //20
 console.log(math.subtract(15, 5)); //10
+
+// ------------------------------------
+
+const cal = require("./calculator");
+
+console.log(cal(13, 6));
