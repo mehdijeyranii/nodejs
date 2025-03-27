b95f5a07962ce6b7f54092109c5c2cedc641f17f
Mehdi Jeyrani <mehdijeyrani@gmail.com>
2025-03-27 21:08:06 +0330

    Export multiple functions using module.exports

--- app.js
+++ app.js
@@ -2,3 +2,10 @@ const add = require("./sum");
 
 const result = add(5, 10);
 console.log(result);
+
+// ------------------------------------
+
+const math = require("./math");
+
+console.log(math.add(15, 5)); //20
+console.log(math.subtract(15, 5)); //10
