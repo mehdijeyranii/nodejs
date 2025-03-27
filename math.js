b7727a552be85546d2528dbc18ea4a2cdd028dfe
Mehdi Jeyrani <mehdijeyrani@gmail.com>
2025-03-27 21:12:23 +0330

    Export multiple functions using exports object

---
+++ math.js
@@ -0,0 +1,16 @@
+// function add(a, b) {
+//   return a + b;
+// }
+// function subtract(a, b) {
+//   return a - b;
+// }
+
+// module.exports = { add, subtract };
+
+exports.add = function (a, b) {
+  return a + b;
+};
+
+exports.subtract = function (a, b) {
+  return a - b;
+};
